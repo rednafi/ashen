36ceac7e0ee83b723c427fae184a0a274ac79ba0
rednafi <redowan.nafi@gmail.com>
2020-03-31 18:27:52 +0600

    Cleaned up

--- app/search_api/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-search_api = Blueprint(
-    "search_api", __name__, template_folder="templates", static_folder="static"
-)
-
-from . import views # isort:skip
--- flask_run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-from dynaconf import settings
-
-application = create_app()
-
-# Uncomment this while testing the app locally
-if settings.ENV_FOR_DYNACONF == "development":
-    application.run(host="0.0.0.0", port=5000, debug=True)
