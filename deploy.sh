e624de525e2cf4c460f86937cddd790f93a5419c
rednafi <redowan.nafi@gmail.com>
2020-04-07 14:52:35 +0600

    Testing with docker

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+# bring down the containers
 docker-compose down
+
+# remove the containers and orphan volumes
+docker-compose rm -f -v
+
+# remove all dangling volumes
+docker volume rm $(docker volume ls -qf dangling=true)
+
+# remove the image of the base app
+# this app will be rebuild
+# ignore the containers that do not need to be rebuilt
+docker rmi $(docker images |grep 'area-search-engine_app')
+
+# bring up the containers again
 docker-compose up -d
