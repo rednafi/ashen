24d7e92ce3ff9924192d3f9a52f12943c2bf94ed
rednafi <redowan.nafi@gmail.com>
2020-04-07 15:55:57 +0600

    Master deployed via CI

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ docker volume rm $(docker volume ls -qf dangling=true)
 # remove the image of the base app
 # this app will be rebuild
 # ignore the containers that do not need to be rebuilt
-docker rmi $(docker images |grep 'area-search-engine_app')
+docker rmi $(docker images | grep 'areasearchengine_app')
 
 # bring up the containers again
 docker-compose up -d
